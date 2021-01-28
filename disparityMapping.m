b99053ce71bd3930958981063fe492f1e4ac56a5
ghartell <77505600+ghartell@users.noreply.github.com>
2021-01-27 23:14:11 -0500

    Add files via upload

--- disparityMapping.m
+++ disparityMapping.m
@@ -1,4 +1,12 @@
-[R1 R2] = rectifyStereoImages(videoFrameLeft, videoFrameRight, stereoParamsTrial4);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% BME70B: Biomedical Engineering Capstone Design
+% Reading in video input from two USB webcams and saving to .avi files
+%
+% Author: Ginette Hartell - 500755250
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Create a disparity map from two stereovision images
+
+[R1 R2] = rectifyStereoImages(left_1, right_1, stereoParams7);
 
 %Create the stereo anaglyph of the rectified stereo pair image and display it. You can view the image in 3-D by using red-cyan stereo glasses.
 A = stereoAnaglyph(R1,R2);
@@ -11,8 +19,8 @@ J1 = rgb2gray(R1);
 J2 = rgb2gray(R2);
 
 %Compute disparity via semiglobal matching
-disparityRange = [0 48];
-disparityMap = disparity(J1,J2,'DisparityRange',disparityRange,'UniquenessThreshold',20);
+disparityRange = [144 176];
+disparityMap = disparity(J1,J2,'DisparityRange',disparityRange,'UniquenessThreshold',30);
 
 %Plot
 figure
